b3b5e69a60c530addbbe6571fc7cf6b784eb9ca0
David Sabalete <dsabalete@gmail.com>
2018-12-28 20:14:37 +0100

    Using defaultProps

--- season-detector/src/index.js
+++ season-detector/src/index.js
@@ -13,7 +13,7 @@ class App extends React.Component {
     );
   }
 
-  render() {
+  renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
       return <div>Error: {this.state.errorMessage}</div>;
     }
@@ -22,7 +22,15 @@ class App extends React.Component {
       return <SeasonDisplay lat={this.state.lat} />;
     }
 
-    return <Spinner/>;
+    return <Spinner message="Please accept location request" />;
+  }
+
+  render() {
+    return (
+      <div>
+       { this.renderContent()}
+      </div>
+    );
   }
 }
 
