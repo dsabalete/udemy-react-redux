5ea6fee713b4e6d2286d533d1568b373d7f48cea
David Sabalete <dsabalete@gmail.com>
2018-12-28 19:35:55 +0100

    Styles and spinner

--- season-detector/src/index.js
+++ season-detector/src/index.js
@@ -1,24 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
-// import SeasonDisplay from "./components/SeasonDisplay";
+import SeasonDisplay from "./components/SeasonDisplay";
+import Spinner from "./components/Spinner";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
+  state = { lat: null, errorMessage: null };
 
-    this.state = { lat: null };
-
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => {
-        console.log(position);
-        this.setState({ lat: position.coords.latitude });
-      },
-      err => console.log(err)
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
     );
   }
 
   render() {
-    return <div>Latitude: {this.state.lat} </div>;
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <Spinner/>;
   }
 }
 
