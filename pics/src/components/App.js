d8fa96ab38493fcc9039d58d7b97371948fa22eb
David Sabalete <dsabalete@gmail.com>
2018-12-29 17:55:42 +0100

    Viewing Request Results

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,11 +1,27 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 import ImageList from "./ImageList";
 
 class App extends React.Component {
-  onSearchSubmit = (term) => {
-    console.log(term);
-  }
+  onSearchSubmit = term => {
+    axios
+      .get("https://api.unsplash.com/search/photos", {
+        params: {
+          query: term
+        },
+        headers: {
+          Authorization:
+            "Client-ID 8697a5426eff67bdbbdff778e5224d83ef7aec385b25f5c284ef2ca2d27ca290"
+        }
+      })
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  };
 
   render() {
     return (
