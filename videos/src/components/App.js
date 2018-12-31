4b279a960b5f8daad51ef6077fd6ffb29355f866
David Sabalete <dsabalete@gmail.com>
2018-12-31 19:44:18 +0100

    Deeply Nested Callbacks

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -1,10 +1,13 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
-import VideoList from './VideoList';
+import VideoList from "./VideoList";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = {
+    videos: [],
+    selectedVideo: null
+  };
 
   onTermSubmit = async term => {
     const response = await youtube.get("search", {
@@ -13,14 +16,21 @@ class App extends React.Component {
       }
     });
 
-    this.setState({ videos: response.data.items })
+    this.setState({ videos: response.data.items });
+  };
+
+  onVideoSelect = video => {
+    console.log("From the App!", video);
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
--- videos/src/components/VideoList.js
+++ videos/src/components/VideoList.js
@@ -1,13 +1,12 @@
 import React from "react";
 import VideoItem from "./VideoItem";
 
-const VideoList = ({ videos }) => {
+const VideoList = ({ videos, onVideoSelect }) => {
   const renderedList = videos.map(video => {
-      console.log(video);
-    return <VideoItem key={video.id.channelId} />;
+    return <VideoItem onVideoSelect={onVideoSelect} video={video} />;
   });
 
-  return <div>{renderedList}</div>;
+  return <div className="ui relaxed divided list">{renderedList}</div>;
 };
 
 export default VideoList;
