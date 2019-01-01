d74eb36e8b5440f3c926446c5e374ed7371faa9a
David Sabalete <dsabalete@gmail.com>
2019-01-01 14:11:02 +0100

    Defaulting Video Selection

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -10,6 +10,10 @@ class App extends React.Component {
     selectedVideo: null
   };
 
+  componentDidMount() {
+    this.onTermSubmit('buildings');
+  }
+
   onTermSubmit = async term => {
     const response = await youtube.get("search", {
       params: {
@@ -17,7 +21,10 @@ class App extends React.Component {
       }
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos: response.data.items,
+      selectedVideo: response.data.items[0]
+    });
   };
 
   onVideoSelect = video => {
