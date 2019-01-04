1136c07251745a00e4539f875459b4766e362183
David Sabalete <dsabalete@gmail.com>
2019-01-05 00:36:09 +0100

    Calling Action Creators from Components

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { selectSong } from "../actions";
 
 class SongList extends Component {
   renderList() {
@@ -7,7 +8,11 @@ class SongList extends Component {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button className="ui button primary">Select</button>
+            <button 
+                className="ui button primary"
+                onClick={() => this.props.selectSong(song)}
+                >
+                Select</button>
           </div>
           <div className="content">{song.title}</div>
         </div>
@@ -16,12 +21,17 @@ class SongList extends Component {
   }
 
   render() {
+    console.log(this.props);
     return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
 
 const mapStateToProps = state => {
+  console.log(state);
   return { songs: state.songs };
 };
 
-export default connect(mapStateToProps)(SongList);
+export default connect(
+  mapStateToProps,
+  { selectSong }
+)(SongList);
