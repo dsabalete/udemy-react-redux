a3ec0a5ef302ef6220f5f01b8ac8cfb5d34fdddc
David Sabalete <dsabalete@gmail.com>
2019-01-06 00:19:24 +0100

    Conditional Rendering

--- songs/src/components/SongDetail.js
+++ songs/src/components/SongDetail.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const SongDetail = props => {
-  if (!props.song) {
-    return <div>No song selected yet</div>
+const SongDetail = ({ song }) => {
+  if (!song) {
+    return <div>Select a song</div>;
   }
 
   return (
     <div>
-      <h3>Title: {props.song.title}</h3>
-      <p>Duration: {props.song.duration}</p>
+      <h3>Details for: </h3>
+      <p>
+        Title: {song.title} 
+        <br />
+        Duration: {song.duration}
+      </p>
     </div>
   );
 };
--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -8,11 +8,12 @@ class SongList extends Component {
       return (
         <div className="item" key={song.title}>
           <div className="right floated content">
-            <button 
-                className="ui button primary"
-                onClick={() => this.props.selectSong(song)}
-                >
-                Select</button>
+            <button
+              className="ui button primary"
+              onClick={() => this.props.selectSong(song)}
+            >
+              Select
+            </button>
           </div>
           <div className="content">{song.title}</div>
         </div>
@@ -21,7 +22,6 @@ class SongList extends Component {
   }
 
   render() {
-    console.log(this.props);
     return <div className="ui divided list">{this.renderList()}</div>;
   }
 }
