b66e8e601aed5a4d480732faf08c3147b30c3a62
David Sabalete <dsabalete@gmail.com>
2019-01-06 23:36:29 +0100

    Dispatching Correct Values

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -4,17 +4,20 @@ import { fetchPosts } from "../actions";
 
 class PostList extends React.Component {
   componentDidMount() {
-      this.props.fetchPosts();
+    this.props.fetchPosts();
   }
 
   render() {
+    console.log(this.props.posts);
     return <div>Post List</div>;
   }
 }
 
+const mapStateToProps = state => {
+  return { posts: state.posts };
+};
+
 export default connect(
-  null,
-  {
-    fetchPosts
-  }
+  mapStateToProps,
+  { fetchPosts }
 )(PostList);
