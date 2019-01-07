341d6f180abdfefda4220cfa6a5cbc91969965d0
David Sabalete <dsabalete@gmail.com>
2019-01-07 18:48:38 +0100

    Extracting Logic to MapStateToProps

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -18,8 +18,8 @@ class PostList extends React.Component {
               <h2>{post.title}</h2>
               <p>{post.body}</p>
             </div>
+            <UserHeader userId={post.userId} />
           </div>
-          <UserHeader userId={post.userId} />
         </div>
       );
     });
