eef6adf45df3cd85c48158864f9fc1339bfe3343
David Sabalete <dsabalete@gmail.com>
2019-01-07 22:34:38 +0100

    Quick refactor with Chain

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,11 +1,14 @@
-import _ from 'lodash';
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 export const fetchPostsAndUsers = () => async (dispatch, getState) => {
   await dispatch(fetchPosts());
 
-  const userIds = _.uniq(_.map(getState().posts, 'userId'));
-  userIds.forEach(id => dispatch(fetchUser(id)));
+  _.chain(getState().posts)
+    .map("userId")
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value();
 };
 
 export const fetchPosts = () => async dispatch => {
@@ -14,7 +17,7 @@ export const fetchPosts = () => async dispatch => {
   dispatch({ type: "FETCH_POSTS", payload: response.data });
 };
 
-export const fetchUser = (id) => async dispatch => {
+export const fetchUser = id => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
 
   dispatch({ type: "FETCH_USER", payload: response.data });
