02369883300791f395788af7590db58fd971c832
David Sabalete Rodríguez <dsabalete@gmail.com>
2022-12-10 16:19:08 +0100

    feat: update season-detector react version

--- season-detector/src/index.js
+++ season-detector/src/index.js
@@ -1,37 +1,35 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import SeasonDisplay from "./components/SeasonDisplay";
-import Spinner from "./components/Spinner";
+import React from 'react'
+import SeasonDisplay from './components/SeasonDisplay'
+import Spinner from './components/Spinner'
+
+import { createRoot } from 'react-dom/client'
+const root = createRoot(document.querySelector('#root')) // createRoot(container!) if you use TypeScript
 
 class App extends React.Component {
-  state = { lat: null, errorMessage: null };
+  state = { lat: null, errorMessage: null }
 
   componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude }),
-      err => this.setState({ errorMessage: err.message })
-    );
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    )
   }
 
   renderContent() {
     if (this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>;
+      return <div>Error: {this.state.errorMessage}</div>
     }
 
     if (!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={this.state.lat} />;
+      return <SeasonDisplay lat={this.state.lat} />
     }
 
-    return <Spinner message="Please accept location request" />;
+    return <Spinner message='Please accept location request' />
   }
 
   render() {
-    return (
-      <div>
-       { this.renderContent()}
-      </div>
-    );
+    return <div>{this.renderContent()}</div>
   }
 }
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+root.render(<App />)
